1e25b23fe956639c5438496742e2ceb41b8031e3
drewc <drewc@users-Mac-mini.local>
2024-02-03 13:59:36 -0700

    Make GCC default for Mac

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -6,49 +6,35 @@ class GerbilScheme < Formula
       using: :git, revision: "92b1a2f642d6ebbcd3bd223ccc0af7ec0d9a42ad"
   version "0.18.1"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
-  revision 2
+  revision 3
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git, branch: "master"
-
-  bottle do
-    root_url "https://github.com/mighty-gerbils/homebrew-gerbil/releases/download/gerbil-scheme-0.18.1_2"
-    rebuild 1
-    sha256 arm64_sonoma: "55dc43ebc5f0851de439e7f6fee505509aaa0c2a7c83898a6329cc02bf858e2c"
-    sha256 ventura:      "73ea365ea9b6796a0146dc613561bad296027610e512d9d7bc536c66b5186c92"
-    sha256 monterey:     "e221d0a0d85c304366579350ab7f65b09e59e821fc4181cbbbae8de29ffd1383"
-    sha256 x86_64_linux: "23f4ff03830ab492d245051ab9b324c8dedba97144fc2eebe20f802b1fa5d63d"
-  end
-
   depends_on "coreutils" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@3"
   depends_on "sqlite"
   depends_on "zlib"
-
   on_macos do
-    depends_on "llvm"
+    depends_on "gcc@13"
   end
-
   on_linux do
     depends_on "gcc@13"
   end
-
   fails_with :gcc do
     version "12" # Select new gcc
     cause "Make it easy with all the same"
   end
-
   def install
     nproc = `nproc`.to_i - 1
 
-    if OS.mac?
-      ENV.prepend_path("PATH", "/usr/local/opt/llvm/bin")
-      ENV.prepend_path("PATH", "/opt/homebrew/opt/llvm/bin")
-    end
+    # if OS.mac?
+    #   ENV.prepend_path("PATH", "/usr/local/opt/llvm/bin")
+    #   ENV.prepend_path("PATH", "/opt/homebrew/opt/llvm/bin")
+    # end
 
-    if OS.linux?
-      ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/bin")
-      ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/sbin")
-    end
+    # if OS.linux?
+    #   ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/bin")
+    #   ENV.prepend_path("PATH", "/home/linuxbrew/.linuxbrew/sbin")
+    # end
 
     ENV["GERBIL_GCC"] = ENV.cc.to_s
     ENV["CC"] = ENV.cc.to_s
