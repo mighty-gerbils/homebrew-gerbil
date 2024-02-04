bc099dfc3c9a578183c17a0f45d15c08256d0c61
Drew Crampsie <me@drewc.ca>
2024-02-04 05:03:43 +0000

    gerbil-scheme: add 0.18.1_3 bottle.

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -8,6 +8,13 @@ class GerbilScheme < Formula
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
   revision 3
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git, branch: "master"
+
+  bottle do
+    root_url "https://github.com/mighty-gerbils/homebrew-gerbil/releases/download/gerbil-scheme-0.18.1_3"
+    sha256 arm64_sonoma: "56b13b3e8862a0d08f647d1708c5311e1dccc9862b10dab38bdf0ebff0dc538e"
+    sha256 ventura:      "2bc2c1f15877a980027181c95fb090c50240c3a132b54b06835723b2ff267686"
+    sha256 monterey:     "fcc12990f5d936e593cfffaba95846926d4638917c66fdfc2b1d73bcb96ece1c"
+  end
   depends_on "coreutils" => :build
   depends_on "pkg-config" => :build
   depends_on "openssl@3"
