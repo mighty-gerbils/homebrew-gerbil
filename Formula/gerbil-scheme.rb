f500cad6aaad8bc573c9765ff55b440e14aa2d52
Drew Crampsie <drewc@Drews-MacBook-Pro.local>
2024-03-22 17:44:44 -0700

    Fix style

--- Formula/gerbil-scheme.rb
+++ Formula/gerbil-scheme.rb
@@ -6,7 +6,7 @@ class GerbilScheme < Formula
       using: :git, revision: "92b1a2f642d6ebbcd3bd223ccc0af7ec0d9a42ad"
   version "0.18.1"
   license any_of: ["LGPL-2.1-or-later", "Apache-2.0"]
-  revision 1
+  # revision 1
   head "https://github.com/mighty-gerbils/gerbil.git", using: :git, branch: "master"
 
   bottle do
@@ -38,11 +38,9 @@ class GerbilScheme < Formula
     ENV["GERBIL_GCC"] = ENV.cc.to_s
     ENV["CC"] = ENV.cc.to_s
     ENV["CXX"] = ENV.cxx.to_s
-    ENV["LDFLAGS"] = '-Wl,-ld_classic'
+    ENV["LDFLAGS"] = "-Wl,-ld_classic"
     ENV["GERBIL_BUILD_CORES"] = nproc.to_s
 
-    system "env"
-    
     system ENV.cc.to_s, "--version"
     system "./configure", "--prefix=#{prefix}", "--enable-march="
     system "make", "-j#{nproc}"
